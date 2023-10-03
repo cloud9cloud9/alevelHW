1d2968fe788cd8f1a95e143eb103c785e30d4d56
cloud9cloud9 <nalezhitiyvlad@gmail.com>
2023-10-03 18:41:44 +0300

    fifth commit

--- src/homew2task5.java
+++ src/homew2task5.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class homew2task5 {
+import java.util.Scanner;
+
+public class homew2task5 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        // отримуємо дані від користувача
+        System.out.print("Введіть число:");
+        String num = scan.next();
+        // записуємо цифри в строку , розвертаємо число , яке отримали в зворотньому порядку
+        String naobor = new StringBuilder(num).reverse().toString();
+        // переводимо строку в числовий тип даних інт
+        int revers = Integer.parseInt(naobor);
+        // виводимо на екран монітору значення, яке отримали1
+        System.out.println(naobor);
+    }
 }
